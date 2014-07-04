bef3001b45d7addda3de7846587de5f8f6ac549f
Rodrigo Viana <rodviana92@gmail.com>
2014-07-04 18:26:01 -0500

    formato contrato  y  modulo usuarios consulta

--- application/views/modulo_usuarios_consulta.php
+++ application/views/modulo_usuarios_consulta.php
@@ -21,11 +21,67 @@
 			    <div id="collapseOne" class="panel-collapse collapse ">
 			      <div class="panel-body">
 			      	<div class="row">
-					  <div class="col-md-4"> <img src="" alt="" class="img-thumbnail img_usuario"></div>
-					  <div class="col-md-4"></div>
-					  <div class="col-md-4"></div>
+					  <div class="col-md-4" style="width: 15% !important;"> 
+					  <img src="" alt="" class="img-thumbnail img_usuario" style="margin-left: 30px; margin-top: 5px;">
+					  </div>
+					  <div class="col-md-4" style="width: 42.5% !important">
+					  	<div class="padre">
+					  			<label>Usuario</label>
+					  			<div style="display: table-cell">  				
+					  		 		<input id="" name="usuario" type="text" class="form-control input_margen" placeholder="Nombre del Usuario" value="">
+								</div>
+							</div>
+							<div class="padre">
+					  			<label>Contraseña</label>
+					  			<div style="display: table-cell">				  				
+					  		 		<input id="" name="nombre" type="password" class="form-control input_margen" placeholder="Password" value="">
+								</div>
+							</div>
+					    </div>
+					  <div class="col-md-4" style="width: 42.5% !important;">
+					  	<div class="padre">
+				  			<label>Nombre del Empleado</label>
+				  			<div style="display: table-cell">				  				
+				  		 		<input id="" name="nombre" type="text" class="form-control input_margen" placeholder="Nombre Empleado" value="">
+							</div>
+						</div>
+							<div class="padre">
+					  			<label>Perfil</label>
+					  			<div style="display: table-cell">
+									<select id="idperfil" name="idperfil" class="form-control input_margen">
+									  <!-- Lista de Opciones de perfil  -->
+									  <option selected="" disabled="">--Seleccione su Perfil--</option>
+									  <option value="1" id="1">
+									   Director General 
+									  </option>
+									  <option value="2" id="2">
+										Director Administrativo 
+									   </option>
+									  <option value="3" id="3">
+										Director Comercial 
+									  </option>
+									  <option value="4" id="4">
+										Gerente Comercial 
+									  </option>
+									  <option value="5" id="5">
+										Community Manager 
+									  </option>
+									  <option value="6" id="6">
+										Diseñador Gráfico Sr 
+									  </option>
+									</select>					  		 		
+								</div>					  
+					   		</div>
+					  </div>
 					</div>
-			      
+					<div class="panel panel-primary">
+					  <div class="panel-heading">
+					    <h3 class="panel-title">Permisos</h3>
+					  </div>
+					  <div class="panel-body">
+					    checbox con permisos , ya sabes!!!
+					  </div>
+					</div>     
 			      </div>
 			    </div>
 			  </div>
